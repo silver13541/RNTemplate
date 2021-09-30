6b344b74c182f950fc72173f07db9c1a660d8042
Elvar Halilov <elvar.halilov@mail.ru>
2021-09-30 11:15:42 +0500

    eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
+import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 import { NavigationContainer } from '@react-navigation/native'
 
 import { RootNavigation } from 'src/navigation/root'
 
 import { AlertProvider } from './modules/alert-context'
-import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client'
 
 const client = new ApolloClient({
   uri: 'https://rickandmortyapi.com/graphql',
